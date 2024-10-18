6fd38d8163ee899f323f72e65686c6c41b03b2b2
Joseph Beeson <joseph@innocraft.com>
2024-10-18 10:11:25 +0200

    nscp add support for downloading files

--- nscp.py
+++ nscp.py
@@ -3,6 +3,7 @@
 
 import os, sys
 from subprocess import Popen, PIPE
+import argparse
 
 import subprocess, shlex
 from threading import Timer
@@ -24,47 +25,81 @@ def run(cmd, timeout_sec=10):
     	
 	return (stdout, stderr, exitcode)
 	
-	
-testonly=False
+parser = argparse.ArgumentParser(description='nscp, multiplexing scp.',
+add_help=True,
+formatter_class=argparse.RawTextHelpFormatter)
 
-if '--test' in sys.argv:
+parser.add_argument('files', default=None, nargs='*', help='files to upload (or to download if --down set)')
+# booleans
+parser.add_argument('--up', action='store_true',   help='upload file (default action)')
+parser.add_argument('--recursive', '-R', action='store_true', help='Add scp recursive flag')
+parser.add_argument('--compress', '-C', action='store_true', help='Add scp compress flag')
+parser.add_argument('--down', action='store_true', help='Download rather than upload')
+parser.add_argument('--test', action='store_true', help="dry run, don't actually do anything")
+
+args = parser.parse_args()
+
+if args.test:
 	print ("")
 	print ("--test specified so not going to actually do anything")
 	print ("")
-	testonly=True
 	
 	
-try:
-	FILE=sys.argv[1]
-except:
-	print ('no file specified')
+if len(args.files) == 0:
+	print ('no files specified')
 	exit (1)
 
-try:
-	REMOTE_PATH=sys.argv[2]
-except:
-	REMOTE_PATH=':'
-
-if REMOTE_PATH[0] != ':':
-	REMOTE_PATH = ':'+REMOTE_PATH
 
 TIMEOUT=int(os.getenv('NSCP_TIMEOUT', 15))
 
-
 data = sys.stdin.readlines()
 
+remote_path='.'
+local_path = ""
+if args.down:
+	if len(args.files) > 2:
+		print ("can only download a single path")
+		exit(1)
+	if len(args.files) == 2:
+		local_path = args.files.pop()
+
+else:
+	if len(args.files) > 1:
+		remote_path = args.files.pop()
+
+scp_args=""
+if args.recursive:
+	scp_args+=" -R "
+
+if args.compress:
+	scp_args+=" -C "
+
 for line in data:
-	cmd = 'scp "{}" {}{}'.format(FILE, line.strip(), REMOTE_PATH)
-	if testonly:
+	host =  line.strip()
+	# default, upload
+	cmd = 'scp {} "{}" {}:{}'.format(scp_args, '" "'.join(args.files), host, remote_path)
+		
+	if args.down:
+		# we're downloading, got to prefix with host to avoid local clobbering
+		if not os.path.isdir(host):
+			os.makedirs(host)
+		local_prefixed_path = "{}/{}".format(host, local_path)
+		if local_prefixed_path.endswith("/"):
+			local_prefixed_path = local_prefixed_path[0:-1]
+
+		cmd = 'scp {} {}:{} {}'.format(scp_args, host, args.files[0], local_prefixed_path)
+	
+	if args.test:
 		print (cmd)
+		continue
+
+	print (line.strip())
+	(out, err, exitcode) = run(cmd, TIMEOUT)
+	if exitcode == 0:
+		print ("OK")
 	else:
-		print (line.strip())
-		(out, err, exitcode) = run(cmd, TIMEOUT)
-		if exitcode == 0:
-			print ("OK")
-		else:
-			print ("exit code {}".format(exitcode))
-			print (err)
+		print ("exit code {}".format(exitcode))
+		print (err)
 		
 	print ("")
 	
